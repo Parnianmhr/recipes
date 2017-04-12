2ab02306fcb336cdc854dee98161d9f88e033aad
Parnian <parnian@parnians-mbp.home>
2017-04-12 12:18:00 +0200

    added title component and test it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Title from './Components/Title'
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <div>
+        <Title content="All Recipes" />
+      </div>
     )
   }
 }
