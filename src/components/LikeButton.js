ea2c15f2a569bbac3606e2655542edea178ab3fa
Parnian <parnian@parnians-mbp.home>
2017-04-19 16:58:17 +0200

    like button test

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,43 +1,51 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent, propTypes } from 'react'
 import './LikeButton.sass'
 import HeartGrey from '../images/heart-grey.svg'
 import HeartRed from '../images/heart-red.svg'
 
 class LikeButton extends PureComponent {
-  constructor() {
-    super()
-    this.state = {
-      liked: false
+  static propTypes = {
+      onChange: PropTypes.func.isRequired,
+      liked: PropTypes.bool.isRequired,
     }
-  }
 
   classNames() {
-    const { liked } = this.state
-    let classes = 'liked'
+    const { liked } = this.props
+    let classes = 'like'
     if (liked) { classes += 'liked' }
     return classes
   }
 
   toggleLike() {
-    this.setState({
-      liked: !this.state.liked
-    })
-      console.log('Like button clicked!')
+    console.log('CLICK (LikeButton)')
+    this.props.onChange()
+  }
+
+  likeStatus() {
+    const { liked, likes } = this.props
+    const likesOtherThanYours = (likes || 0) - 1
+    if (liked && likesOtherThanYours > 0) {
+      return `You and ${likesOtherThanYours} others like this`
     }
+    if (liked) return 'You like this'
+    if (likes > 0) return `${likes} others like this`
+    return null
+  }
+
 
   render() {
-      const { liked } = this.state
-      return (
-        <p className={ this.classNames() }>
-          <button onclick={ this.toggleLike.bind(this) }>
+    const { liked } = this.props
+    return (
+      <p className={ this.classNames() }>
+        <button onClick={ this.toggleLike.bind(this) }>
           <img className="heart" src={ liked ? HeartRed : HeartGrey } />
           <span className="copy">
             <img className="heart" src={ liked ? HeartRed : HeartGrey } />
           </span>
-          </button>
-          <span className="likes">{ liked ? 'You like this' : null }</span>
-        </p>
-      )
+        </button>
+        <span className="likes">{this.likeStatus()}</span>
+      </p>
+    )
   }
 }
 export default LikeButton
