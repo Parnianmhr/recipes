c8ec5423ee18ca818679f8e235e5022e0fcc856e
Parnian <parnian@parnians-mbp.home>
2017-04-26 12:39:31 +0200

    UI materials installed and configured

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 import './App.sass'
+// Material UI Components
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import muiTheme from './assets/styles/theme'
 
 class App extends Component {
+  static childContextTypes = {
+      muiTheme: React.PropTypes.object.isRequired,
+    }
+
+    getChildContext() {
+      return { muiTheme }
+    }
+
   render() {
     return (
-      <div className="app">
-        { this.props.children }
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div className="app">
+          <Navigation />
+          <Loading />
+          { this.props.children }
+          <LoadError />
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { Router, Route, IndexRoute } from 'react-router'
 import store, { history } from './store'
-
+import injectTapEventPlugin from 'react-tap-event-plugin'
 import App from './App'
 import RecipesContainer from './recipes/RecipesContainer'
 import RecipePage from './recipes/RecipePage'
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
 
 ReactDOM.render(
   <Provider store={store}>
